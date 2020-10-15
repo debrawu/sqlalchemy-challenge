bc5be05c8be81fac4c5adff65b14f3da73438546
debrawu <dwu1124@gmail.com>
2020-10-14 19:45:42 -0500

    working on flask file

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session  
 from sqlalchemy import create_engine, func  
+import pandas as pd
 
 #SET UP THE DATABASE
 
@@ -39,8 +40,7 @@ def home():
         f'Precipitation: /api/v1.0/precipitation<br/>'
         f'Stations: /api/v1.0/stations<br/>'
         f'Temperature Observations: /api/v1.0/tobs<br/>'
-        f'/api/v1.0/<start><br/>'
-        f'/api/v1.0/<start>/<end><br/>'
+        f'Temperature Stats with a Start - End Date (YYYY-MM-DD): /api/v1.0/temp/<start>/<end><br/>'
     )
 # Convert the query results to a dictionary using date as the key and prcp as the value.
 @app.route("/api/v1.0/precipitation")
@@ -88,26 +88,72 @@ def temp():
     session = Session(engine)
 
     #get the last year info 
-    last_date_in_df = session.query(Measurement.date).order_by(Measurement.date.desc()).first[0] 
-    last_date_in_df = dt.date(2017, 8, 23)
-    yr_from_last = last_date_in_df - dt.timedelta(days=365)
+    # last_date_in_df = session.query(Measurement.date).order_by(Measurement.date.desc()).first[0] 
+    yr_from_last = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
-    #get the most active station
-    most_active = session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).first()
+    # #get the most active station
+    # most_active = session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).first()
 
     #filter out the data for the last year in df from the station USC00519281
-    results = session.query(Measurement.tobs, Measurement.date).filter(Measurement.date > yr_from_last).filter(Measurement.station == 'USC00519281').all()
+    results = session.query(Measurement.tobs).\
+    filter(Measurement.station == 'USC00519281').\
+    filter(Measurement.date >= yr_from_last).all()
 
     session.close()
 
-    tobs = []
+    # tobs = []
+    # for result in results:
+    #     column = {}
+    #     column["tobs"] = result.tobs
+    #     column["date"] = result.date
+    #     tobs.append(column)
+
+    # return jsonify(tobs)
+    
+    # Unravel results into a 1D array and convert to a list
+    temps = list(np.ravel(results))
+    # Return the results
+    return jsonify(temps=temps)
+
+    #omg this is not working, but i tried it in jupyter lab and it's working - WHAT WHY
+
+@app.route('/api/v1.0/<start_input>')
+@app.route('/api/v1.0/<start_input>/<end_input>')
+# <start_input> 'March-03-20'
+# start_input.split(-) ['March', '03', 20']
+# _[0] Month
+def start(start_input, end_input=None):
+    'retrieving min, max and average temps for all dates greater than the start date'
+    #create session
+    # try: 
+    # end_date=dt.datetime.strptime(end_input, '%B-%y-%d')
+#     end_input=dt.datetime.strf(end_date, '%Y-%m-%d')
+    end_date=pd.to_datetime(end_input)
+    end_input=end_date.strftime('%Y-%m-%d')
+    #except: 
+
+    session = Session(engine)
+    # end_input='2018-08-01'
+    if end_input: # if end_input has value. if true, end_input has value
+        results=session.query(func.min(Measurement.tobs).label('min'), func.max(Measurement.tobs).label('max'), func.avg(Measurement.tobs).label('average'))
+        filtered_results=results.filter(Measurement.date >= start_input).filter(Measurement.date <= end_input).all()
+    else:  # which means there is not end_input
+        results=session.query(func.min(Measurement.tobs).label('min'), func.max(Measurement.tobs).label('max'), func.avg(Measurement.tobs).label('average'))
+        filtered_results=results.filter(Measurement.date >= start_input).all()
+    session.close()
+
+    temp_stats_start = []
     for result in results:
         column = {}
-        column["tobs"] = result.tobs
-        column["date"] = result.date
-        tobs.append(column)
+        column['min'] = result.min
+        column['max'] = result.max
+        column['average'] = result.average
+        temp_stats_start.append(column)
+    return jsonify(temps=temp_stats_start)
+    #this isn't working either whyyy
 
-    return jsonify(tobs)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
